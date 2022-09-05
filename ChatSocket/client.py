652fbd5069e6bb73b9f1b9ebd8a53c2f10f682e8
Victor Andrade <101683741+victorandradee@users.noreply.github.com>
2022-09-05 18:40:23 -0300

    Add files via upload

---
+++ ChatSocket/client.py
@@ -0,0 +1,48 @@
+from socket import *
+from threading import *
+from tkinter import *
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+hostIp = "192.168.82.254"
+portNumber = 32166
+
+clientSocket.connect((hostIp, portNumber))
+
+window = Tk()
+window.title("Conectado a: "+ hostIp+ ":"+str(portNumber))
+
+txtMessages = Text(window, width=50)
+txtMessages.grid(row=0, column=0, padx=10, pady=10)
+
+txtYourMessage = Entry(window, width=50)
+txtYourMessage.insert(0,"Sua mensagem")
+txtYourMessage.grid(row=1, column=0, padx=10, pady=10)
+
+def sendMessage():
+    clientMessage = txtYourMessage.get()
+    txtMessages.insert(END, "\n" + "Você: "+ clientMessage)
+    clientSocket.send(clientMessage.encode("utf-8"))
+
+btnSendMessage = Button(window, text="Send", width=20, command=sendMessage)
+btnSendMessage.grid(row=2, column=0, padx=10, pady=10)
+
+def recvMessage():
+    while True:
+        serverMessage = clientSocket.recv(1024).decode("utf-8")
+        print(serverMessage)
+        txtMessages.insert(END, "\n"+serverMessage)
+
+recvThread = Thread(target=recvMessage)
+recvThread.daemon = True
+recvThread.start()
+
+def exit():
+    """Encerrar a conexão"""
+    msg = "quit"
+    clientSocket.send(bytes(msg, "utf8"))
+    clientSocket.close()
+    window.quit()
+
+window.mainloop()
